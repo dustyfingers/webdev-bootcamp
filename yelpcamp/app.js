70565c1e5cf95115f454e186c9960904996a7095
Louie Williford <dustyfingers01257@gmail.com>
2019-04-16 23:16:13 -0600

    added ability to edit and delete campgrounds

--- yelpcamp/app.js
+++ yelpcamp/app.js
@@ -4,6 +4,7 @@ var express = require('express'),
     mongoose = require('mongoose'),
     passport = require('passport'),
     LocalStrategy = require('passport-local'),
+    methodOverride = require('method-override'),
     passportLocalMongoose = require('passport-local-mongoose'),
     bodyParser = require('body-parser'),
     Campground = require('./models/campground'),
@@ -22,6 +23,7 @@ mongoose.connect('mongodb://localhost/yelp_camp', { useNewUrlParser: true });
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(__dirname + '/public'));
+app.use(methodOverride('_method'));
 // // seed database with dummy data
 // seedDB();
 
--- yelpcamp/routes/campgrounds.js
+++ yelpcamp/routes/campgrounds.js
@@ -59,6 +59,44 @@ router.get('/:id', (req, res) => {
 });
 
 
+// edit campground route
+router.get('/:id/edit', (req, res) => {
+  Campground.findById(req.params.id, (err, foundCampground) => {
+    if (err) {
+      console.log(err);
+      res.redirect('/campgrounds');
+    } else {
+      res.render('campgrounds/edit', { campground: foundCampground});
+    }
+  });
+});
+
+
+// update campground route
+router.put('/:id', (req, res) => {
+  // find and update the correct campground
+  Campground.findByIdAndUpdate(req.params.id, req.body.campground, (err, updatedCampground) => {
+    if (err) {
+      res.redirect('/campgrounds');
+    } else {
+      res.redirect('/campgrounds/' + req.params.id);
+    }
+  });
+});
+
+
+// destroy campground route
+router.delete('/:id', (req, res) => {
+  Campground.findByIdAndRemove( req.params.id, (err) => {
+    if (err) {
+      res.redirect('/campgrounds');
+    } else {
+      res.redirect('/campgrounds');
+    }
+  });
+});
+
+
 // isLoggedIn middleware
 function isLoggedIn(req, res, next) {
   if(req.isAuthenticated()) {
