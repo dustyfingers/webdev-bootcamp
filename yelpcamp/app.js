0ac3a975f00486882c042b03bc7abd866a435ea3
Louie Williford <dustyfingers01257@gmail.com>
2019-04-18 01:31:48 -0600

    adding flash messages

--- yelpcamp/app.js
+++ yelpcamp/app.js
@@ -3,6 +3,7 @@ var express = require('express'),
     port = 9898,
     mongoose = require('mongoose'),
     passport = require('passport'),
+    flash = require('connect-flash'),
     LocalStrategy = require('passport-local'),
     methodOverride = require('method-override'),
     passportLocalMongoose = require('passport-local-mongoose'),
@@ -24,6 +25,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(__dirname + '/public'));
 app.use(methodOverride('_method'));
+app.use(flash());
 // // seed database with dummy data
 // seedDB();
 
@@ -41,6 +43,8 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 app.use((req, res, next) => {
   res.locals.currentUser = req.user;
+  res.locals.error = req.flash('error');
+  res.locals.success = req.flash('success');
   next();
 });
 
--- yelpcamp/middleware/index.js
+++ yelpcamp/middleware/index.js
@@ -10,17 +10,20 @@ middlewareObj.checkCampgroundOwnership = (req, res, next) => {
   if (req.isAuthenticated()) {
     Campground.findById(req.params.id, (err, foundCampground) => {
       if (err) {
+        req.flash('error', 'Campground not found...');
         res.redirect('back');
       } else {
         // does user own campground?
         if (foundCampground.author.id.equals(req.user._id)) {
           next();
         } else {
+          req.flash('error', "Permission denied");
           res.redirect('back');
         }
       }
     });
   } else {
+    req.flash('error', 'You need to be logged in to do that');
     res.redirect('back');
   }
 }
@@ -50,6 +53,7 @@ middlewareObj.isLoggedIn = (req, res, next) => {
   if(req.isAuthenticated()) {
     return next();
   }
+  req.flash('error', 'You need to be logged in to do that');
   res.redirect('/login');
 }
 
